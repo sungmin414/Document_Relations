1282978378ca752b7369a3f6648f61175f9f7d2d
sungmin414 <sungmin3231@gmail.com>
2019-04-09 19:04:47 +0900

    ABC의 관계필드에서 related_name에 %(class)s 를 사용해서 동적으로 값 지정

--- app/abstract_base_classes/models/related.py
+++ app/abstract_base_classes/models/related.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 __all__ = (
-    'Person',
-    'PhotoBase',
-    'TextBase',
+    'RelatedUser',
+    'PhotoPost',
+    'TextPost',
     'PostBase',
 )
 
 
-class Person(models.Model):
+class RelatedUser(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -17,11 +17,13 @@ class Person(models.Model):
 
 class PostBase(models.Model):
     author = models.ForeignKey(
-        Person,
+        RelatedUser,
         on_delete=models.CASCADE,
         # 유저(Person) 입장에서
         # 자신이 특정 Post의 author인 경우에 해당하는 모든 PostBase 객체를 참조
-        related_name='posts',
+        related_name='%(class)s_set',
+        related_query_name='%(class)s'
+
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -29,14 +31,18 @@ class PostBase(models.Model):
         abstract = True
 
 
-class PhotoBase(PostBase):
+class PhotoPost(PostBase):
+    # author의 related_name
+    # photopost_set
     photo_url = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return f'Post (author: {self.author.name})'
 
 
-class TextBase(PostBase):
+class TextPost(PostBase):
+    # author의 related_name
+    # textpost_set
     text = models.TextField(blank=True)
 
     def __str__(self):
